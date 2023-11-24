ab04f46cf305164873d937184330daf6001df850
leedthanh <leedthanh@gmail.com>
2023-11-24 05:37:14 -0600

    fix side bar

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import explore  # Assuming the script is named explore.py
 import prediction as predict_explore
 import prediction as heart
-page = st.sidebar.selectbox('Select page: ', ('Use Machine Learning','Explore The Dataset'))
+page = st.selectbox('Select page: ', ('Use Machine Learning','Explore The Dataset'))
 
 if page == 'Use Machine Learning':
     heart.run()
